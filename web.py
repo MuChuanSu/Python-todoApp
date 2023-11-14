2ed1eecd0aa362fa0f295513f7265814028d616b
xzaxza <xzaxza12@gmail.com>
2023-11-14 01:42:15 +0000

    add delete function

--- web.py
+++ web.py
@@ -5,17 +5,25 @@ import functions
 todos = functions.getTodos()
 def add_todo():
     todo = st.session_state["new_todo"]
-    '''This line gets the user input with the key "new_todo"' ''
+    '''This line gets the user input with the key "new_todo"'''
     todos.append(todo+'\n')
     functions.writeTodos(todos)
+
 
 
 
 st.title("My Todo App")
 st.subheader("This is my Todo App")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.writeTodos(todos)
+        del st.session_state[todo]
+        '''delete the key pair'''
+        st.experimental_rerun()
+
 
 
 
